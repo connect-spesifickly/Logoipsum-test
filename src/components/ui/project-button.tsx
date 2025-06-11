45b1b782f1942a54c1eece09df03de64269682fe
connect-spesifickly <mozar.sosial@gmail.com>
2025-06-11 10:14:28 +0700

    create button component for global use

---
+++ src/components/ui/project-button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Button } from "./button";
+
+export default function ProjectButton() {
+  return (
+    <div>
+      <Button className="bg-blue-600 h-[40px] py-2 px-4 rounded-md text-sm"></Button>
+    </div>
+  );
+}
